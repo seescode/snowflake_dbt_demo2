62259260251f608efa47a6e750c41f604ec8186f
Yong Choi <seesyong@gmail.com>
2026-03-20 11:55:25 -1000

    Add additional IDs to my_first_dbt_model (#9)
    * Add additional IDs to my_first_dbt_model
    
    * Eliminate duplicate entry in my_first_dbt_model
    
    Removed duplicate entry for id 1 in source_data.

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -12,6 +12,12 @@ with source_data as (
 
     select 1 as id
     union all
+    select 2 as id
+    union all
+    select 3 as id
+    union all
+    select 4 as id
+    union all
     select null as id
 
 )
