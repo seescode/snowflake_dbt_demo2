b1ad9ff67c09371ad74f15184b091d4653c92e31
Yong Choi <seesyong@gmail.com>
2026-03-20 11:36:44 -1000

    Add additional IDs to my_first_dbt_model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -12,6 +12,14 @@ with source_data as (
 
     select 1 as id
     union all
+    select 1 as id
+    union all
+    select 2 as id
+    union all
+    select 3 as id
+    union all
+    select 4 as id
+    union all
     select null as id
 
 )
