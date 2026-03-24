812e108f49924817bfa6c99bc4b100a8c7231de1
Yong Choi <seesyong@gmail.com>
2026-03-24 06:49:18 -1000

    this is great

---
+++ models/example/new_thing.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select 1 as id
+    union all
+    select 2 as id
+    union all
+    select 3 as id
+
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
